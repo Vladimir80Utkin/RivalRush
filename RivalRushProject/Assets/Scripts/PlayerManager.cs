ed99ed058617f144923c69e15b919cf3c8ce4bb4
Vladimir80Utkin <vova.utkin.04@mail.ru>
2024-05-31 12:24:00 +0300

    Delet Demo + adding PlayerManager and PlayerController(test)

---
+++ RivalRushProject/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Photon.Pun;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    PhotonView PV;
+
+    private void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+    
+    void Start()
+    {
+        if (PV.IsMine)
+        {
+            CreateController();
+        }
+    }
+
+    void CreateController()
+    {
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), Vector3.zero, quaternion.identity);
+    }
+}
