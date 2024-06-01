cacb8ae96ea538fbea1cd22ba9e4ba64dfdbcedc
Vladimir80Utkin <107009358+Vladimir80Utkin@users.noreply.github.com>
2024-06-02 02:13:02 +0300

    Player Movement + idle/walk/jump animations

--- RivalRushProject/Assets/Scripts/PlayerController.cs
+++ RivalRushProject/Assets/Scripts/PlayerController.cs
@@ -7,16 +7,33 @@ public class PlayerController : MonoBehaviour
 {
   [SerializeField] private float walkSpeed = 2f;
   [SerializeField] private float runSpeed;
-  [SerializeField] private float jumpForce;
+  
+  [SerializeField] private float jumpForce = 7f;
+  [SerializeField] private bool jumpControl;
+  [SerializeField] private int jumpIteration = 0;
+  [SerializeField] private int jumpValueIteration = 60;
+  
 
   private float horizontalInput;
 
   private Vector2 moveVector;
   private Rigidbody2D rb;
 
+  [SerializeField] private Animator anim;
+
+  private bool faceRight = true;
+  
+  [SerializeField] private bool onGround;
+  [SerializeField] private Transform groundCheck;
+  [SerializeField] private float checkRadius = 0.5f;
+  [SerializeField] private LayerMask Ground;
+  
+
   private void Awake()
   {
     rb = GetComponent<Rigidbody2D>();
+    anim = GetComponent<Animator>();
+    groundCheck = transform.Find("groundCheck");
   }
 
   private void Update()
@@ -24,6 +41,7 @@ public class PlayerController : MonoBehaviour
     Walk();
     Jump();
     SetDirection();
+    CheckGround();
   }
 
   private void FixedUpdate()
@@ -35,11 +53,25 @@ public class PlayerController : MonoBehaviour
   {
     moveVector.x = Input.GetAxis("Horizontal");
     rb.velocity = new Vector2(moveVector.x * walkSpeed, rb.velocity.y);
+    
+    anim.SetFloat("moveX",Math.Abs(moveVector.x));
   }
 
   private void Jump()
   {
-    
+    if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
+    {
+      if (onGround) { jumpControl = true; }
+    }
+    else { jumpControl = false; }
+
+    if (jumpControl) {
+      if (jumpIteration++ < jumpValueIteration)
+      {
+        rb.AddForce(Vector2.up * jumpForce / jumpIteration);
+      }
+    }
+    else {jumpIteration = 0;}
   }
 
   private void Sit()
@@ -49,6 +81,16 @@ public class PlayerController : MonoBehaviour
 
   private void SetDirection()
   {
-    
+    if ((moveVector.x > 0 && !faceRight) || (moveVector.x < 0 && faceRight))
+    {
+      transform.localScale *= new Vector2(-1, 1);
+      faceRight = !faceRight;
+    }
+  }
+
+  void CheckGround()
+  {
+    onGround = Physics2D.OverlapCircle(groundCheck.position, checkRadius,Ground);
+    anim.SetBool("onGround",onGround);
   }
 }
