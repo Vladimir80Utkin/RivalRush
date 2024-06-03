8cf39f567b378f74f0bdfb3a2886f69e17e5c6ec
Vladimir80Utkin <107009358+Vladimir80Utkin@users.noreply.github.com>
2024-06-03 10:26:04 +0300

    Changing Menu Interface

--- RivalRushProject/Assets/Scripts/PlayerController.cs
+++ RivalRushProject/Assets/Scripts/PlayerController.cs
@@ -1,115 +1,145 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviourPunCallbacks, IPunObservable
 {
   [SerializeField] private float walkSpeed = 2f;
-  [SerializeField] private float runSpeed = 10f;
-  [SerializeField] private float realSpeed;
-  
-  [SerializeField] private float jumpForce = 7f;
+    [SerializeField] private float runSpeed = 10f;
+    [SerializeField] private float realSpeed;
 
-  private int jumpCount = 0;
-  [SerializeField] private int maxJumpValue = 2;
-  
+    [SerializeField] private float jumpForce = 7f;
 
-  private float horizontalInput;
+    private int jumpCount = 0;
+    [SerializeField] private int maxJumpValue = 2;
 
-  private Vector2 moveVector;
-  private Rigidbody2D rb;
+    private float horizontalInput;
+    private Vector2 moveVector;
+    private Rigidbody2D rb;
 
-  [SerializeField] private Animator anim;
+    [SerializeField] private Animator anim;
 
-  private bool faceRight = true;
-  
-  [SerializeField] private bool onGround;
-  [SerializeField] private Transform groundCheck;
-  [SerializeField] private float checkRadius = 0.5f;
-  [SerializeField] private LayerMask Ground;
-  
-  public int coins = 0;
+    private bool faceRight = true;
 
-  private void Awake()
-  {
-    rb = GetComponent<Rigidbody2D>();
-    anim = GetComponent<Animator>();
-    groundCheck = transform.Find("groundCheck");
+    [SerializeField] private bool onGround;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float checkRadius = 0.5f;
+    [SerializeField] private LayerMask Ground;
 
-    realSpeed = walkSpeed;
-  }
+    public int coins = 0;
 
-  private void Update()
-  {
-    Walk();
-    Run();
-    Jump();
-    SetDirection();
-    CheckGround();
-  }
-
-  private void Walk()
-  {
-    moveVector.x = Input.GetAxis("Horizontal");
-    rb.velocity = new Vector2(moveVector.x * realSpeed, rb.velocity.y);
-    
-    anim.SetFloat("moveX",Math.Abs(moveVector.x));
-  }
-
-  void Run()
-  {
-    if (Input.GetKey(KeyCode.LeftShift) && onGround)
+    private void Awake()
     {
-      anim.SetBool("run", true);
-      realSpeed = runSpeed;
-    }
-    else
-    {
-      anim.SetBool("run", false);
-      realSpeed = walkSpeed;
-    }
-  }
-  
-  private void Jump()
-  {
-    if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) && (onGround || (++jumpCount < maxJumpValue)))
-    {
-      rb.AddForce(Vector2.up * jumpForce);
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        groundCheck = transform.Find("groundCheck");
+
+        realSpeed = walkSpeed;
     }
 
-    if (onGround) { jumpCount = 0; }
-  }
-
-  private void Sit()
-  {
-    
-  }
-
-  private void SetDirection()
-  {
-    if ((moveVector.x > 0 && !faceRight) || (moveVector.x < 0 && faceRight))
+    private void Update()
     {
-      transform.localScale *= new Vector2(-1, 1);
-      faceRight = !faceRight;
+        if (photonView.IsMine)
+        {
+            Walk();
+            Run();
+            Jump();
+            SetDirection();
+            CheckGround();
+        }
+        else
+        {
+            // Обновляем анимацию на основе полученных данных
+            anim.SetFloat("moveX", Mathf.Abs(moveVector.x));
+            anim.SetBool("run", realSpeed == runSpeed);
+            anim.SetBool("onGround", onGround);
+        }
     }
-  }
 
-  void CheckGround()
-  {
-    onGround = Physics2D.OverlapCircle(groundCheck.position, checkRadius,Ground);
-    anim.SetBool("onGround",onGround);
-  }
+    private void Walk()
+    {
+        moveVector.x = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveVector.x * realSpeed, rb.velocity.y);
 
-  public void AddCoin(int amount)
-  {
-    coins += amount;
-    // Здесь можно добавить логику для отображения количества монет на UI и т.д.
-  }
+        anim.SetFloat("moveX", Mathf.Abs(moveVector.x));
+    }
 
-  public void RemoveCoin(int amount)
-  {
-    coins -= amount;
-    // Здесь можно добавить логику для отображения количества монет на UI и т.д.
-  }
+    private void Run()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && onGround)
+        {
+            anim.SetBool("run", true);
+            realSpeed = runSpeed;
+        }
+        else
+        {
+            anim.SetBool("run", false);
+            realSpeed = walkSpeed;
+        }
+    }
+
+    private void Jump()
+    {
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) && (onGround || (++jumpCount < maxJumpValue)))
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+
+        if (onGround) { jumpCount = 0; }
+    }
+
+    private void SetDirection()
+    {
+        if ((moveVector.x > 0 && !faceRight) || (moveVector.x < 0 && faceRight))
+        {
+            transform.localScale *= new Vector2(-1, 1);
+            faceRight = !faceRight;
+        }
+    }
+
+    private void CheckGround()
+    {
+        onGround = Physics2D.OverlapCircle(groundCheck.position, checkRadius, Ground);
+        anim.SetBool("onGround", onGround);
+    }
+
+    public void AddCoin(int amount)
+    {
+        coins += amount;
+        // Здесь можно добавить логику для отображения количества монет на UI и т.д.
+    }
+
+    public void RemoveCoin(int amount)
+    {
+        coins -= amount;
+        // Здесь можно добавить логику для отображения количества монет на UI и т.д.
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            // Отправляем данные другим игрокам
+            stream.SendNext(moveVector.x);
+            stream.SendNext(realSpeed);
+            stream.SendNext(onGround);
+            stream.SendNext(faceRight);
+        }
+        else
+        {
+            // Получаем данные от других игроков
+            moveVector.x = (float)stream.ReceiveNext();
+            realSpeed = (float)stream.ReceiveNext();
+            onGround = (bool)stream.ReceiveNext();
+            bool newFaceRight = (bool)stream.ReceiveNext();
+
+            if (newFaceRight != faceRight)
+            {
+                transform.localScale *= new Vector2(-1, 1);
+                faceRight = newFaceRight;
+            }
+        }
+    }
 }
--- RivalRushProject/Assets/Scripts/ShootingArm.cs
+++ RivalRushProject/Assets/Scripts/ShootingArm.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
-public class ShootingArm : MonoBehaviour
+public class ShootingArm : MonoBehaviourPun
 {
     [SerializeField] private float offset;
     [SerializeField] private GameObject bullet;
@@ -14,15 +15,27 @@ public class ShootingArm : MonoBehaviour
 
     private void Update()
     {
+        if (photonView.IsMine)
+        {
+            HandleAiming();
+            HandleShooting();
+        }
+    }
+
+    private void HandleAiming()
+    {
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f,0f, rotZ + offset);
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+    }
 
+    private void HandleShooting()
+    {
         if (timeBtwShots <= 0)
         {
             if (Input.GetMouseButtonDown(0))
             {
-                Instantiate(bullet, shotPoint.position, transform.rotation);
+                photonView.RPC("Shoot", RpcTarget.All, shotPoint.position, transform.rotation);
                 timeBtwShots = startTimeBtwShots;
             }
         }
@@ -30,5 +43,11 @@ public class ShootingArm : MonoBehaviour
         {
             timeBtwShots -= Time.deltaTime;
         }
+    }
+
+    [PunRPC]
+    private void Shoot(Vector3 position, Quaternion rotation)
+    {
+        Instantiate(bullet, position, rotation);
     }
 }
